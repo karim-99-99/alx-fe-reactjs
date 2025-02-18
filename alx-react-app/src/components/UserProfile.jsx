9b4bc049ab2b04544a45d96c0783f68e398a0064
karim-99-99 <kareem.hossam99@yahoo.com>
2025-02-18 12:29:36 +0200

    kk

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -3,10 +3,9 @@ const UserProfile = (props) => {
   return (
     <div>
       <h2>{props.name}</h2>
-      <p>{props.age}</p>
-      <p>{props.bio}</p>
+      <p>AGE: {props.age}</p>
+      <p>Bio: {props.bio}</p>
     </div>
-    
   );
 };
 
