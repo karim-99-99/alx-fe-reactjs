48c71dc19bb015c644b2f74fb18c4f0d06bc16a7
karim-99-99 <kareem.hossam99@yahoo.com>
2025-03-02 15:22:09 +0200

    Added alx-react-app project11

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Services from "./Services";
-import Contact from "./Contact";
+import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 
 function App() {
