8f94622c7499c06aaa0e1de26cd046467add464d
karim-99-99 <kareem.hossam99@yahoo.com>
2025-03-07 14:09:26 +0200

    Added alx-react-app project11

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,22 +2,23 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import RecipeList from "./componant/RecipeList";
-import AddRecipeForm from "./componant/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import RecipeDetails from "./componant/RecipeDetails";
-import Home from "./componant/Home";
+import RecipeDetails from "./components/RecipeDetails";
+import Home from "./components/Home";
 const App = () => (
+  <div>
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
     </Routes>
   </Router>
-  //   <div>
-  //   <RecipeList />
-  //   <AddRecipeForm />
-  // </div>
+    
+    <RecipeList />
+    <AddRecipeForm />
+  </div>
 );
 
 export default App;
