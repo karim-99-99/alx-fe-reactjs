8edc70f0322120be2f17a67e45a5ef22e4b8b6f5
karim-99-99 <kareem.hossam99@yahoo.com>
2025-03-09 14:56:34 +0200

    Added alx-react-app project11

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -7,17 +7,21 @@ import AddRecipeForm from "./components/AddRecipeForm";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeDetails from "./components/RecipeDetails";
 import Home from "./components/Home";
+import SearchBar from "./components/SearchBar ";
+import AdvancedFilters from "./components/AdvancedFilters";
 const App = () => (
   <div>
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
-    </Routes>
-  </Router>
-{/*     
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
+
+    <SearchBar />
+    <AdvancedFilters />
     <RecipeList />
-    <AddRecipeForm /> */}
+    <AddRecipeForm />
   </div>
 );
 
