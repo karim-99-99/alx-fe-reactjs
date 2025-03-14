98572a70665a80917b750ddcbde2217a767c17ab
karim-99-99 <kareem.hossam99@yahoo.com>
2025-03-14 22:26:59 +0200

    Add1

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import PostsComponent from './PostsComponent';
+import PostsComponent from './components/PostsComponent';
 
 const queryClient = new QueryClient();
 
