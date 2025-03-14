1d3f289c9da449b5c00012ce96b1c9b3eabeeb72
karim-99-99 <kareem.hossam99@yahoo.com>
2025-03-14 16:06:40 +0200

    Add1

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import RegistrationForm from './components/RegistrationForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <RegistrationForm />
+    <formikForm />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
