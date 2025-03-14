d3a40734576d740a2b02497ad3f061f09d150197
karim-99-99 <kareem.hossam99@yahoo.com>
2025-03-14 15:59:02 +0200

    Add1

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const RegistrationForm = () => {
     const [input,setInput] =useState( { username: '' , email: '' , password: ''})
-    
+    const[error,setError] = useState({});
     const handleChange = (e) => {
 const {name , value} = e.target;
 setInput(prevState=> ({...prevState,[name]: value}));
@@ -10,8 +10,21 @@ setInput(prevState=> ({...prevState,[name]: value}));
 
     const handleSubmit= () => {
         e.preventDefault();
+        let newErrors = {}
+
+        if(!input.username.trim()) {
+            newErrors.username = "Username is required";
+        }
+        if(!input.email.trim()) {
+            newErrors.email = "email is required";
+        }
+        if(!input.password.trim()) {
+            newErrors.password= "password is required"
+        }
+        setError(newErrors);
         console.log(input);
     };
+  
 
     return (
         <form onSubmit={handleSubmit}>
@@ -21,18 +34,21 @@ setInput(prevState=> ({...prevState,[name]: value}));
             value={username}
             onChange={handleChange}
             />
+            {error.username && <p>{error.username}</p>}
             <input 
             type="email"
             name="email"
             value={email}
             onChange={handleChange}
             />
+             {error.email && <p>{error.email}</p>}
             <input 
             type="password"
             name="password"
             value={password}
             onChange={handleChange}
             />
+             {error.password && <p>{error.password}</p>}
             <button type="submit">Submit</button>
         </form>
 
