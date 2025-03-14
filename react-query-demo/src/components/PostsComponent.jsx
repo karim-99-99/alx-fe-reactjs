14a9785e55c0e235af1c44da8178f166e088d46a
karim-99-99 <kareem.hossam99@yahoo.com>
2025-03-14 22:43:13 +0200

    Add1

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -10,8 +10,8 @@ const ReactQuery = () => {
 //handle Data Fetching and Caching
 
 const { data, isError, isLoading } = useQuery({
-    queryKey: ["fetchData"], 
-    queryFn: fetchData,
+    queryKey: ["fetchPosts"], 
+    queryFn: fetchPosts,
     staleTime: 1000 * 60 * 5, // Data remains fresh for 5 minutes
     cacheTime: 1000 * 60 * 10, // Cache remains for 10 minutes
     refetchOnWindowFocus: false, // Prevent refetching when switching tabs
@@ -26,6 +26,8 @@ if(isError) return <div> Error loading data</div>
 //Render the fetch data
 return (
     <div>
+        <h> Fetch Posts</h>
+        <button onClick={() => refetch} disabled={isFetching}>{isFetching ? "Refreshing..." : "Refetch Posts"}</button>
         {data.map(item => (
             <div key={item.id}>{item.title}</div>
         ))}
