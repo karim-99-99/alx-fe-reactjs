64eebe179e6dc5617e89be62a413096138eb5d70
karim-99-99 <kareem.hossam99@yahoo.com>
2025-03-21 02:31:59 +0200

    Add1

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -13,13 +13,13 @@ const HomePage = () => {
   return (
     <div>
       <h1 className="text-5xl font-bold mb-7">Recipes List</h1>
-      <div className="flex flex-centre justify-center gap-20 sm:max-w-3xl  ">
+      <div className="grid sm:grid-cols-2 justify-center gap-20 sm:max-w-3xl">
         {recipes.map((recipe) => (
           <div key={recipe.id} className="shadow-lg bg-slate-100 rounded-sm ">
             <img
               src={recipe.image}
               alt={recipe.title}
-              className="max-h-60 rounded-lg mx-auto sm:gap-14"
+              className="max-h-60 rounded-lg mx-auto sm:gap-14 hover:scale-110 transition-transform duration-300 ease-in-out"
             />
             <h2 className="font-bold text-violet-500 m-2 text-xl"> {recipe.title} </h2>
             <p> {recipe.summary} </p>
