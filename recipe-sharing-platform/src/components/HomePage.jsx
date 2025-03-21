31ecdefbf3f9966bda5878677ca02560c30164c2
karim-99-99 <kareem.hossam99@yahoo.com>
2025-03-21 02:22:59 +0200

    Add1

---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+const HomePage = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then((responce) => responce.json())
+      .then((data) => setRecipes(data))
+      .catch((error) => console.log("Error loading Data", error));
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-5xl font-bold mb-7">Recipes List</h1>
+      <div className="flex flex-centre justify-center gap-20 sm:max-w-3xl  ">
+        {recipes.map((recipe) => (
+          <div key={recipe.id} className="shadow-lg bg-slate-100 rounded-sm ">
+            <img
+              src={recipe.image}
+              alt={recipe.title}
+              className="max-h-60 rounded-lg mx-auto sm:gap-14"
+            />
+            <h2 className="font-bold text-violet-500 m-2 text-xl"> {recipe.title} </h2>
+            <p> {recipe.summary} </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
